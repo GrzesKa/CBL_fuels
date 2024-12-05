1f2cfa6446d4412f9712e1d7554f16c468862795
Yasmine Fredj <y.fredj@student.tue.nl>
2024-12-05 19:32:22 +0100

    Gamma function completed with aROHR function

--- Example Data Set/Example Data Set/Simple.m
+++ Example Data Set/Example Data Set/Simple.m
@@ -340,11 +340,6 @@ smooth_p = sgolayfilt(filtered_averaged_p, 1, 9);
 % Use the average volume (since geometry doesn't change)
 V_avg = mean(V, 2); % Average over all cycles
 
-%% Compute aROHR
-%gamma = 1.2; % Update gamma if needed
-
-%aROHR = (gamma / (gamma - 1)) * smooth_p .* smooth_dVdCa + (1 / (gamma - 1)) * V_avg .* smooth_dpdCa;
-
 %% Plot aROHR
 %f3 = figure(3);
 %set(f3, 'Position', [400 400 800 400]); % Figure size
@@ -357,11 +352,26 @@ V_avg = mean(V, 2); % Average over all cycles
 
 
 % Interpolate gamma for the given crank angles in Ca(:, 1)
-gamma_interp = interp1(Gamma_at_angle(:, 1), Gamma_at_angle(:, 2), Ca(:, 1), 'linear', 'extrap');
+
+gamma_start = Gamma_at_angle(1, 2);  % First gamma value
+gamma_end = Gamma_at_angle(end, 2); % Last gamma value
+
+gamma_full = zeros(size(Ca(:, 1)));  % Initialize the output array
+
+% Crank angles less than the first
+gamma_full(Ca(:, 1) < Gamma_at_angle(1, 1)) = gamma_start;
+
+% Crank angles greater than the last
+gamma_full(Ca(:, 1) > Gamma_at_angle(end, 1)) = gamma_end;
+
+% Crank angles within the range
+in_range = (Ca(:, 1) >= Gamma_at_angle(1, 1)) & (Ca(:, 1) <= Gamma_at_angle(end, 1));
+gamma_full(in_range) = interp1(Gamma_at_angle(:, 1), Gamma_at_angle(:, 2), Ca(in_range, 1), 'linear');
+
 
 % Compute aROHR using the interpolated gamma
-aROHR = (gamma_interp ./ (gamma_interp - 1)) .* smooth_p .* smooth_dVdCa + ...
-        (1 ./ (gamma_interp - 1)) .* V_avg .* smooth_dpdCa;
+aROHR = (gamma_full ./ (gamma_full - 1)) .* smooth_p .* smooth_dVdCa + ...
+        (1 ./ (gamma_full - 1)) .* V_avg .* smooth_dpdCa;
 
 %% Plot aROHR
 f3 = figure(3);
